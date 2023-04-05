f3dd721e6f16a07ca9dbe64da0eadad5714fe27d
Nikhil Messa <85455453+NikhilMessa@users.noreply.github.com>
2023-04-05 15:10:00 +0530

    Add files via upload

---
+++ Light.c
@@ -0,0 +1,35 @@
+int GREEN = 2;
+int YELLOW = 3;
+int RED = 4;
+int DELAY_GREEN = 5000;
+int DELAY_YELLOW = 2000;
+int DELAY_RED = 5000;
+void loop()
+{
+  green_light();
+  delay(DELAY_GREEN);
+  yellow_light();
+  delay(DELAY_YELLOW);
+  red_light();
+  delay(DELAY_RED);
+}
+void green_light()
+{
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(RED, LOW);
+}
+
+void yellow_light()
+{
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, HIGH);
+  digitalWrite(RED, LOW);
+}
+
+void red_light()
+{
+  digitalWrite(GREEN, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(RED, HIGH);
+}
